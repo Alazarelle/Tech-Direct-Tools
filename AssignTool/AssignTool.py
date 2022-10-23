b2eb45d6e71a4889eaf40a30075c266f9548ed74
Alazarelle <alazarelle@gmail.com>
2022-10-23 16:37:31 +1100

    Pretty Much Done

--- AssignTool/AssignTool.py
+++ AssignTool/AssignTool.py
@@ -10,42 +10,59 @@ objname = filename.split('_')[0]
 folderpath = '/'.join(filepath.split('/')[:-1])+'/'
 wipLocation = filepath.split('/')[-6]
 pubFolderpath = folderpath.split(wipLocation)[0]+'publish'+folderpath.split(wipLocation)[1]
+global warned
 warned = False
 
-
-def getLocalSurfacers():
-    #Should already be checked but just in case
-    if fileFormatCheck("surfacing"):
-        publishSurfaces()
+#global shaderList
+#global ShaderVersList
 
 def publishSurfaces():
-    #get scene objects
-    theNodes = cmds.ls(dag = True, s = True, o = True)
-    list = json.loads('{"geo":"shader"}') 
-    #Go through scene
-    for shade in theNodes:
-        #Geometry
-        geo = shade.split('Shape')[0]
-        shadeEng = cmds.listConnections(shade , type = 'shadingEngine')
-        #don't get no shader objects
-        if shadeEng is None:
-            #ignore (TO DO: Is there a better ay to do the reverse of this?)
-            print("ignoring "+geo)
+    if fileFormatCheck("surfacing"):
+        #get scene objects
+        theNodes = cmds.ls(dag = True, s = True, o = True)
+        list = {} 
+        #Go through scene
+        for shade in theNodes:
+            #Geometry
+            geo = shade.split('Shape')[0]
+            shadeEng = cmds.listConnections(shade , type = 'shadingEngine')
+            #don't get no shader objects
+            if shadeEng is None:
+                #ignore
+                print("ignoring "+geo)
+            else:
+                material = cmds.ls(cmds.listConnections(shadeEng), materials = True)
+                val={str(geo):str(material[0])}
+                list.update(val)
+        checkPrevFileAndSave(list)
+    else:
+        cmds.confirmDialog( title='Warning', message="This is not a surfacing file. Please open a surfacing file to publish surfacers.")
+
+    
+def newVersFormat(obj, type):
+    if obj == "":
+        return "001"
+    else:
+        if (type == 'json'):
+            vers = int(obj.split('_')[2].split('.')[0].split('v')[1])+1
         else:
-            material = cmds.ls(cmds.listConnections(shadeEng), materials = True)
-            val={str(geo):str(material[0])}
-            list.update(val)
-    checkPrevFileAndSave(list)
-    
-def newVersFormat(objName):
-    print(objName)
-    print(objName.split('.')[1].split('v')[1])
-    return str(int(objName.split('.')[1].split('v')[1])+1)
+            vers = int(obj.split('.')[1].split('v')[1])+1
+        #verions
+    if vers < 10:
+        return "00" + str(vers)
+    elif vers < 100:
+        return "0" + str(vers)
+    return str(vers)
             
-    
 
 def checkPrevFileAndSave(list):
-    #TO DO: need to create a check for folders/create (material, source, textures)
+    #path checks (create if they don't exist
+    if (os.path.exists(pubFolderpath+'material/') == False):
+        os.makedirs(pubFolderpath+'material/')
+    if (os.path.exists(pubFolderpath+'source/') == False):
+        os.makedirs(pubFolderpath+'source/')
+    if (os.path.exists(pubFolderpath+'textures/') == False):
+        os.makedirs(pubFolderpath+'textures/') 
     file_list=os.listdir(pubFolderpath+'material/')
     #Check for Previous JSON files
     for file in file_list:
@@ -53,65 +70,50 @@ def checkPrevFileAndSave(list):
             fileName = file
     #msg for created files
     msg = ""
-    if 'fileName' in locals():
-        #Check if updates have been made
-        if sameJson(pubFolderpath+'material/'+fileName, list) == False:
-            #MAYA SHADER
-            file_list=os.listdir(pubFolderpath+'material/')
-            #Check for existing file
-            for file in file_list:
-                if "shader." in file:
-                    shaderFile = file
-            shaderName = objname+"_shader.v"+newVersFormat(shaderFile)
-            cmds.file(rename = pubFolderpath+'material/'+shaderName)
-            cmds.file(s=True,f=True)
-            msg += "\nVersion "+newVersFormat(shaderFile)+" Shader file made."
-            
-            #MAYA SOURCE
-            file_list=os.listdir(pubFolderpath+'source/')
-            #Check for existing maya surface file
-            for file in file_list:
-                if "surface" in file:
-                    surfaceFile = file
-            surfaceName = objname+"_surface.v"+newVersFormat(surfaceFile)
-            cmds.file(rename = pubFolderpath+'source/'+surfaceName)
-            cmds.file(s=True,f=True)
-            #ALSO WIP MAYA FILE
-            cmds.file(rename = folderpath+objname+"_surface.v"+newVersFormat(filepath))
-            cmds.file(s=True,f=True)
-            msg+="\nVersion "+newVersFormat(surfaceFile)+" Surface maya file made."
-            
-            #JSON
-            version = int(fileName.split('_')[2].split('.')[0].split('v')[1])
-            with open(pubFolderpath+'material/'+objname+'_shaderList'+'_v'+str(version+1)+'.json', "w") as outfile:
-                json.dump(list, outfile)
-            msg+="\nVersion "+str(version+1)+" Geo/Shade Json file made."
-            
-            #Success Message
-            cmds.confirmDialog( title='Success', message=msg)
-        else:
-            cmds.confirmDialog( title='Warning', message="No changes to shaders made.")
-    else:
-        #FIRST MAYA SHADER
-        shaderName = objname+"_shader.v001.mb"
-        cmds.file(rename = pubFolderpath+'material/'+shaderName)
-        cmds.file(s=True,f=True)
-        msg+="\nFirst version shader published file made."
+    #If first time creating
+    if 'fileName' not in locals():
+        fileName = shaderFile = surfaceFile = ""
+    #Create files + check for ShaderList updates
+    if (fileName == "" or sameJson(pubFolderpath+'material/'+fileName, list) == False):
 
-        #FIRST MAYA SOURCE FILE
-        surfaceName = objname+"_surface.v001.mb"
+        #MAYA SHADER
+        file_list=os.listdir(pubFolderpath+'material/')
+        #Check for existing file
+        for file in file_list:
+            if "shader." in file:
+                shaderFile = file
+        shaderName = objname+"_shader.v"+newVersFormat(shaderFile, "shader")
+        #select shaders to be exported
+        shaders = []
+        for geo, shader in list.items():
+            shaders.append(shader)
+        cmds.select(shaders)
+        cmds.file(rename = pubFolderpath+'material/'+shaderName)
+        cmds.file(f=True, op= "v=0;", typ = "mayaBinary", pr=True, es=True)
+        msg += "\nVersion "+newVersFormat(shaderFile, "shader")+" Shader file made."
+        
+        #MAYA SOURCE
+        file_list=os.listdir(pubFolderpath+'source/')
+        #Check for existing maya surface file
+        for file in file_list:
+            if "surface" in file:
+                surfaceFile = file
+        surfaceName = objname+"_surface.v"+newVersFormat(surfaceFile, "surface")
         cmds.file(rename = pubFolderpath+'source/'+surfaceName)
         cmds.file(s=True,f=True)
         #ALSO WIP MAYA FILE
-        cmds.file(rename = folderpath+objname+"_surface.v"+newVersFormat(filepath))
+        cmds.file(rename = folderpath+objname+"_surface.v"+newVersFormat(filepath, "surface"))
         cmds.file(s=True,f=True)
-        msg+="\nFirst version source maya published file made."
+        msg+="\nVersion "+newVersFormat(surfaceFile, "surface")+" Surface maya file made."
         
-        #FIRST JSON
-        with open(pubFolderpath+'material/'+objname+'_shaderList_v001.json', "w") as outfile:
+        #JSON
+        with open(pubFolderpath+'material/'+objname+'_shaderList_v'+newVersFormat(fileName, "json")+'.json', "w") as outfile:
             json.dump(list, outfile)
-        msg+="\nFirst version of shader json List published file made."
+        msg+="\nVersion "+newVersFormat(fileName, "json")+" Geo/Shade Json file made."
+        #Success Message
         cmds.confirmDialog( title='Success', message=msg)
+    else:
+        cmds.confirmDialog( title='Warning', message="No changes to shaders made.")
 
 def sameJson(oldfile, new):
     old = json.load(open(oldfile))
@@ -131,77 +133,73 @@ def getSelShaderPath():
     else:
         cmds.confirmDialog( title='Warning', message="Nothing selected. Please select object.")
         return ""
-    
+        
+def GetSurfaceList():
+    clearList()
+    shaderList = []
+    jsonArray = []
+    shaderFolderpath = getSelShaderPath()
+    if shaderFolderpath != "":
+        if os.path.exists(shaderFolderpath):
+            file_list=os.listdir(shaderFolderpath)
+            #check that path has material folder/exists
+            for file in file_list:
+                if "json" in file:
+                    jsonArray.append(file)
+        if jsonArray == []:
+            cmds.confirmDialog( title='Warning', message="No shaderlist exists.")
+        else:
+            for vers in jsonArray:
+                shaderList = cmds.menuItem(p='shaderVersList', label = vers)
 
-def setSurfaces(version):
+
+def setSurfaces():
     if fileFormatCheck("light"):
-        #get surface model path
-        shaderFolderpath = getSelShaderPath()
-        #Some paths don't work
-        #if shaderFolderpath 
-        if shaderFolderpath != "":
-            if version == "latest":
-                file_list=os.listdir(shaderFolderpath)
-                jsonArray = []
-                #check that path has material folder/exists
-                for file in file_list:
-                    if "json" in file:
-                        jsonArray.append(file)
-                #Check if this object's JSON shaderlist exists
-                if jsonArray != []:
-                    jsonFile = jsonArray[-1]
-                    #Show latest vers in textbox
-                    cmds.textField("Version", edit=True, tx=jsonFile)
-                    list = json.load(open(shaderFolderpath+jsonFile))
+        warned = False
+        #get Shaderlist
+        shaderVers = cmds.optionMenu('shaderVersList', q=True, v=True)
+        if shaderVers is None:
+            cmds.confirmDialog( title='Warning', message="No Object and Shaders selected.")
+        else:
+            theNodes = cmds.ls(dag = True, s = True, o = True)
+            sceneGeo = ""
+            #Go through scene
+            for shade in theNodes:
+                #Geometry
+                geo = shade.split('Shape')[0]
+                if "mRef" in geo:
+                    sceneGeo = geo.split(':')[0]+":"
+            #get path
+            shaderFolderpath = getSelShaderPath()
+            shaderList = json.load(open(shaderFolderpath+shaderVers))
+            #get and import associated shader file
+            file_list=os.listdir(shaderFolderpath)
+            obj = shaderVers.split('_')[0]
+            vers = shaderVers.split('v')[-1].split('.')[0]
+            namespace = shaderVers.split('.')[0]
+            for file in file_list:
+                if "shader." in file:
+                    if vers in file:
+                        shaderFile = file
+            importedShaders = cmds.file(shaderFolderpath+shaderFile, i=True, mergeNamespacesOnClash=True, namespace=namespace)
+            for geo, shader in shaderList.items():
+                #Select proper Geo and assign shader
+                if cmds.ls(sceneGeo+geo) == [] and warned == False:
+                    print(sceneGeo+geo)
+                    warned = True
+                    cmds.confirmDialog( title='Warning', message="This object's model and/or names are not set up correctly for shaders.")
                 else:
-                    list = ""
-            elif version == "older":
-                #get JSON input vers from textfield
-                jsonFile = "armchair01_shaderList_v3.json"
-                #get surface model path
-                shaderFolderpath = getSelShaderPath()
-                print(shaderFolderpath+jsonFile)
-                list = json.load(open(shaderFolderpath+jsonFile))
-            #Check for ShaderlList
-            if list != "":
-                #get specific files that match shaderlist
-                version = jsonFile.split('_')[2].split('.')[0].split('v')[1]
-                #shader file
-                file_list=os.listdir(shaderFolderpath)
-                shaderFile = ''
-                for file in file_list:
-                    if "shader." in file:
-                        if version in file:
-                            shaderPath = shaderFolderpath+file
-                #source file (USEFUL???)
-                sourceFile = ''
-                sourceFolderpath = '/'.join(shaderFolderpath.split('/')[:-2])+'/source/'
-                file_list=os.listdir(sourceFolderpath)
-                for file in file_list:
-                    if "surface" in file:
-                        if version in file:
-                            sourcePath = sourceFolderpath+file
-                #Import shaders
-                cmds.file(shaderPath, i=True, mergeNamespacesOnClash=True)
-                #Loop through List and apply shaders
-                for geo, shader in list.items():
-                    print(geo)
-                    #cmds.select(geo)
-                    print(shader)
-                    selected = cmds.ls(sl=True,long=True)
-                    selected.append(shader)
-                    #unShaded.append(objects[i])
-                    #where geo == this file's geo
-                    #where shader == shaderSources' shader
-                    #assign
-            else:
-                cmds.confirmDialog( title='Warning', message="No shaderlist exists.")            
+                    print(len(cmds.ls(sceneGeo+geo)))
+                    print(sceneGeo+geo)
+                    cmds.select(cmds.ls(sceneGeo+geo)[0])
+                    cmds.hyperShade(assign = (namespace+":"+shader))
+
     else:
         cmds.confirmDialog( title='Warning', message="This is not a lighting file. Please open a lighting file to assign surfacers.")
     
 
 def fileFormatCheck(type):
-    global warned
+    warned = False
     #get file directory and folder
     filepath = cmds.file(q=True, sn=True)
     fileType = filepath.split('/')[-2]
@@ -217,25 +215,33 @@ def fileFormatCheck(type):
     else:
         return False
         
+        
+def clearList():
+    menuItems = cmds.optionMenu('shaderVersList', q=True, itemListLong=True)
+    if menuItems:
+        cmds.deleteUI(menuItems)
+        
 def assignTool():
     if cmds.window('assignTool', exists = True):
         cmds.deleteUI('assignTool', window = True)
         
-    cmds.window('assignTool', title='assignTool', resizeToFitChildren=True)
-    
+    cmds.window('assignTool', title='Shader Tool', widthHeight = (250, 300), sizeable = False, resizeToFitChildren = True)
     with LayoutManager(cmds.columnLayout(adj=True, rowSpacing=5)) as col:
+        cmds.separator(h=20)
+        cmds.text("SHADER ASSIGNER", font="boldLabelFont")
+        #Surfacing
         isSurfacing = fileFormatCheck("surfacing") 
         cmds.separator(h=20)
         cmds.text('Surfacing', en = isSurfacing)
-        cmds.button(label = 'Publish Shaders', command = 'getLocalSurfacers()', en = isSurfacing)
-        SurfaceText = cmds.textField(en=False, tx="" )
+        cmds.button(label = 'Publish Shaders', command = 'publishSurfaces()', en = isSurfacing)
         cmds.separator(h=20)
-        
+        #Lighting
         isLighting = fileFormatCheck("light") 
         cmds.text('Lighting', en = isLighting)
-        cmds.button(l = 'Set Selected Latest Shader', command = 'setSurfaces("latest")', en = isLighting)
-        shaderList = cmds.textField('Version', en = isLighting)
-        cmds.button(l = 'Set Selected Version Shader', command = 'setSurfaces("older")', en = isLighting)
+        cmds.button(l = 'Get Selected Shaders', command = 'GetSurfaceList()', en = isLighting)
+        cmds.optionMenu('shaderVersList', en = isLighting)
+        shaderList = ""
+        cmds.button(l = 'Set Selected Version Shader', command = 'setSurfaces()', en = isLighting)
         cmds.separator(h=20)
         if warned == False:
             cmds.showWindow('assignTool')
